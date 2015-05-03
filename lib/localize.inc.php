c6444d6d94c8e1510baa2928906f7669d218d78e
icy-wolf <nicole.lawrence93@gmail.com>
2015-05-03 18:40:40 -0400

    Trying to connect to database

--- lib/localize.inc.php
+++ lib/localize.inc.php
@@ -18,8 +18,8 @@ return function(Site $site) {
     $site->setRoot('');
     //mysql:host=mysql-user.cse.msu.edu;dbname=lawre272
     //mysql:host=localhost;dbname=lawre272
-    $site->dbConfigure('mysql:host=localhost;dbname=lawre272',
-        'root',       // Database user
-        '',     // Database password
+    $site->dbConfigure('mysql:host=mysql-user.cse.msu.edu;dbname=lawre272',
+        'lawre272',       // Database user
+        'A42278766',     // Database password
         'p2_');            // Table prefix
 };
